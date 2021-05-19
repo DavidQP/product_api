ca4522ab76a7500376b6b20119be423c3f1ed665
David Quintero <juda9323@gmail.com>
2021-05-18 19:17:03 -0500

    Agregando cambios en las rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'api/products/dashboards', to: 'dashboards#index'
-  get 'api/products/dashboards/new', to: 'dashboards#new'
-  post 'api/products/dashboards', to: 'dashboards#create'
+  get 'api/dashboards', to: 'dashboards#index'
+  get 'api/dashboards/new', to: 'dashboards#new'
+  post 'api/dashboards', to: 'dashboards#create'
   #post 'api/products/load', to: 'products#load'
   get 'api/products', to: 'products#index'
   get 'api/products/:id', to: 'products#show'
